64feea2e48a46bc417ff2bc2a1ca39123a574f5f
Shivam Shishangia <shivams@arizona.edu>
2024-06-17 12:44:58 -0700

    Updated API fetching using Alamofire

---
+++ 20. ToDoAPI/ToDoAPi/Pods/Target Support Files/Pods-ToDoAPi/Pods-ToDoAPi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ToDoAPi : NSObject
+@end
+@implementation PodsDummy_Pods_ToDoAPi
+@end
--- 20. ToDoAPI/ToDoAPi/ToDoAPi/Network/APIHelper.swift
+++ 20. ToDoAPI/ToDoAPi/ToDoAPi/Network/APIHelper.swift
@@ -5,7 +5,7 @@
 //  Created by Shivam Shishangia on 12/06/24.
 //
 
-import Foundation
+import Alamofire
 
 class APIHelper {
     static let shared = APIHelper()
@@ -13,21 +13,16 @@ class APIHelper {
     private init() {}
 
     func fetchToDoData<T: Decodable>(model: T.Type, apiURL: URL, completion: @escaping (T?) -> Void) {
-        URLSession.shared.dataTask(with: apiURL) { data, response, error in
-            guard error == nil else {
-                fatalError(Constants.Errors.error.rawValue + "\(String(describing: error))")
-            }
-
-            guard let data = data else {
+        AF.request(apiURL).response { response in
+            guard let data = response.data else {
                 fatalError(Constants.Errors.fetchDataError.rawValue)
             }
-
             do {
                 let apiData = try JSONDecoder().decode(T.self, from: data)
                 completion(apiData)
             } catch {
                 completion(nil)
             }
-        }.resume()
+        }
     }
 }
