5d98b4109d9ace84c39479810d4b45cc480cd51d
Shivam Shishangia <shivams@arizona.edu>
2024-07-01 08:34:51 -0700

    ExpandableTableView

---
+++ 26. ExpandableTableView/ExpandableTableView/ExpandableTableView/ExpandableTableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableTableViewApp.swift
+//  ExpandableTableView
+//
+//  Created by Shivam Shishangia on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableTableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
