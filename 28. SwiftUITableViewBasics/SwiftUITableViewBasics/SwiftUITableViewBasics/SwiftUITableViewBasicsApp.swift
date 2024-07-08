8f3690b2e6c78d46ad41288657db86767edbd547
Shivam Shishangia <shivams@arizona.edu>
2024-07-08 07:57:57 -0700

    SwiftUITableView

---
+++ 28. SwiftUITableViewBasics/SwiftUITableViewBasics/SwiftUITableViewBasics/SwiftUITableViewBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITableViewBasicsApp.swift
+//  SwiftUITableViewBasics
+//
+//  Created by Shivam Shishangia on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITableViewBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 28. SwiftUITableViewBasics/SwiftUITableViewBasics/SwiftUITableViewBasics/TableView.swift
@@ -0,0 +1,66 @@
+//
+//  TableView.swift
+//  SwiftUITableViewBasics
+//
+//  Created by Shivam Shishangia on 03/07/24.
+//
+
+import SwiftUI
+
+struct TableView: View {
+    @State var isSelectedArray = Array(repeating: false, count: 100)
+
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button("Color All", action: {
+                    isSelectedArray = Array(repeating: true, count: 100)
+                })
+                
+                Spacer()
+                
+                Button("Reset All", action: {
+                    isSelectedArray = Array(repeating: false, count: 100)
+                })
+                
+                Spacer()
+            }.padding()
+
+            List {
+                ForEach(isSelectedArray.indices, id: \.self) { index in
+                    Text(isSelectedArray[index] ? "Hello selected \(index + 1)" : String(index + 1))
+                        .onTapGesture {
+                            isSelectedArray[index].toggle()
+                        }
+                        .padding(.horizontal)
+                        .listRowBackground(backgroundColor(for: index + 1, isSelected: isSelectedArray[index]))
+                }
+            }
+            .listStyle(.plain)
+        }
+        .padding(.vertical)
+    }
+    
+    func backgroundColor(for number: Int, isSelected: Bool) -> Color {
+        guard isSelected else {
+            return Color.white
+        }
+        if number % 2 == 0 && number % 5 == 0 {
+            return Color.green
+        } else if number % 2 == 0 {
+            return Color.yellow
+        } else if number % 5 == 0 {
+            return Color.red
+        } else {
+            return Color.white
+        }
+    }
+}
+
+struct TableView_Previews: PreviewProvider {
+    static var previews: some View {
+        TableView()
+    }
+}
